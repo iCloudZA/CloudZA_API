b4daf22f067c65457989ab663cf672288b62ae2e
iCloudZA <admin@osuu.cc>
2023-01-08 15:46:27 +0800

    重构404页面样式🛠️

--- admin/404.php
+++ admin/404.php
@@ -1,14 +1,12 @@
-<div class=" bg-body-extra-light">
+<div class="hero bg-body-extra-light block block-rounded" style="height: auto;">
     <div class="hero-inner">
         <div class="content content-full">
             <div class="py-4 text-center">
-                <div class="display-1 fw-bold text-danger">
-                    <i class="fa fa-exclamation-triangle opacity-50 me-1"></i> 404
-                </div>
-                <h1 class="fw-bold mt-5 mb-2">哎呀..你刚刚发现了一个错误页面..</h1>
-                <h2 class="fs-4 fw-medium text-muted mb-5">很抱歉，找不到您要查找的页面。</h2>
-                <a class="btn btn-lg btn-alt-secondary" data-pjax href="./">
-                    <i class="fa fa-arrow-left opacity-50 me-1"></i> 返回首页
+                <i class="si si-ghost text-primary display-3"></i>
+                <h1 class="h2 fw-bold mt-4 mb-2">Not Found</h1>
+                <h2 class="h5 fw-medium text-muted mb-5">Please return to the console.</h2>
+                <a class="btn btn-primary fs-sm" data-pjax href="./?index">
+                    <i class="fa fa-arrow-left opacity-50 me-1"></i> 返回控制台
                 </a>
             </div>
         </div>
