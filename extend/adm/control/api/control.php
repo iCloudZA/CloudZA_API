ad891d0b3487a2f4a1f4ad1f3add447e7e9b522f
iCloudZA <admin@osuu.cc>
2023-01-08 23:41:58 +0800

    修复了bug

--- extend/adm/control/api/control.php
+++ extend/adm/control/api/control.php
@@ -1,7 +1,9 @@
 <?php
-    if($act = 'delSelect'){
-        exit(ReturnSuccess('删除成功，但并没有执行SQL'));
-    }
-    if($act = 'delApi'){
-        exit(ReturnSuccess('执行成功，但并没有删除'));
-    }
+if(!isset($islogin))header("Location: /");//非法访问
+if ($act === 'delSelect') {
+    exit(ReturnSuccess('删除成功，但并没有执行SQL'));
+}
+
+if ($act === 'delapi') {
+    exit(ReturnSuccess('执行成功,但并没有删除'));
+}
