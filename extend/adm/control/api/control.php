0d85710fc3d72d09d22b4b38e0b67987e9cdf080
iCloudZA <admin@osuu.cc>
2023-01-09 21:21:20 +0800

    新增了添加API的接口,但没有执行sql的功能💥

--- extend/adm/control/api/control.php
+++ extend/adm/control/api/control.php
@@ -1,9 +1,16 @@
 <?php
 if(!isset($islogin))header("Location: /");//非法访问
+// 添加
+if($act === 'add'){
+
+    exit(ReturnSuccess('添加成功'));
+}
+
+// 删除选中
 if ($act === 'delSelect') {
     exit(ReturnSuccess('删除成功，但并没有执行SQL'));
 }
-
+// 删除按钮
 if ($act === 'delapi') {
     exit(ReturnSuccess('执行成功,但并没有删除'));
 }
