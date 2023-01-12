9ac73128d2a53526de99240051dd198c5b79f132
iCloudZA <admin@osuu.cc>
2023-01-12 22:55:31 +0800

    新增网站设置

--- extend/adm/set/api/set.php
+++ extend/adm/set/api/set.php
@@ -5,19 +5,36 @@ Name:系统配置API
 */
 if (!isset($islogin)) header("Location: /"); //非法访问
 
-if ($act == 'admin') {
-    $data = array(
-        'web_title' => $_POST['web_title'],
-        'web_des' => $_POST['web_des'],
-        'web_key' => $_POST['web_key'],
-        'web_beian' => $_POST['web_beian'],
-        'web_ban' => $_POST['web_ban'],
-        'page_enums' => $_POST['page_enums'],
-    );
-    $perform_sql = $db->table('web_set')->update(['web_title' => $data['web_title'], 'web_des' => $data['web_des'], 'web_key' => $data['web_key'], 'web_beian' => $data['web_beian'], 'web_ban' => $data['web_ban'], 'page_enums' => $data['page_enums']]);
-    if ($perform_sql) {
-        echo json(200, '修改成功');
+if($act == 'web') {
+    $data[ 'web_title' ] = isset($_POST[ 'web_title' ]) && !empty($_POST[ 'web_title' ]) ? purge($_POST[ 'web_title' ]) : '';
+    $data[ 'web_des' ] = isset($_POST[ 'web_des' ]) && !empty($_POST[ 'web_des' ]) ? purge($_POST[ 'web_des' ]) : '';
+    $data[ 'web_key' ] = isset($_POST[ 'web_key' ]) && !empty($_POST[ 'web_key' ]) ? purge($_POST[ 'web_key' ]) : '';
+    $data[ 'web_qq' ] = isset($_POST[ 'web_qq' ]) && !empty($_POST[ 'web_qq' ]) ? purge($_POST[ 'web_qq' ]) : '';
+    $data[ 'web_beian' ] = isset($_POST[ 'web_beian' ]) && !empty($_POST[ 'web_beian' ]) ? purge($_POST[ 'web_beian' ]) : '';
+    $data[ 'web_ban' ] = isset($_POST[ 'web_ban' ]) && !empty($_POST[ 'web_ban' ]) ? purge($_POST[ 'web_ban' ]) : '';
+    $data[ 'web_page_nums' ] = isset($_POST[ 'web_page_nums' ]) && !empty($_POST[ 'web_page_nums' ]) ? purge($_POST[ 'web_page_nums' ]) : '';
+    if ( !f($data)) exit(ReturnError('数据不完整'));
+    $table = Db::table('webset');
+    $origData = $table->find();
+    // 对比原始数据
+    if (array_diff($origData , (array)$data) && array_diff((array)$data , $origData)) {
+        if ($table->update($data)) {
+            exit(ReturnSuccess('更新成功'));
+        } else {
+            exit(ReturnError('更新失败'));
+        }
     } else {
-        echo json(201, '修改失败');
+        exit(ReturnError('你没有修改任何内容！'));
+    }
+}
+function f ($arr): bool
+{
+    $len1 = count($arr);
+    $newarr = array_filter($arr);
+    $len2 = count($newarr);
+    if ($len1 === $len2) {
+        return true;
+    } else {
+        return false;
     }
 }
--- extend/adm/set/view/web.php
+++ extend/adm/set/view/web.php
@@ -5,6 +5,7 @@ Hidden:false
 Name:网站设置
 Url:set_web
 */
+$row = Db::table('webset')->find();
 ?>
 <div class="col-12">
     <div class="block block-rounded">
@@ -16,37 +17,77 @@ Url:set_web
 <div class="block block-rounded">
     <div class="block-header block-header-default">
         <h3 class="block-title">
-            <i class="fa fa-gear me-1 text-muted"></i> SEO设置
+            <i class="fa fa-gear me-1 text-muted"></i> 网站设置
         </h3>
     </div>
     <div class="block-content">
-        <form id="edit-profile" onsubmit="return false;">
             <div class="row items-push">
-                <div class="col-lg-3">
-                    <p class="text-muted">
-                        你可以在这里修改网站的SEO信息
-                    </p>
-                </div>
-                <div class="col-lg-7 offset-lg-1">
+                <div class="col-12 ">
                     <div class="mb-4">
-                        <label class="form-label">网站标题</label>
-                        <input class="form-control form-control-lg" id="title" name="title" placeholder="输入网站标题" type="number" value="">
+                        <label class="form-label" for="title">网站标题</label>
+                        <input class="form-control form-control-lg" id="title" name="title" placeholder="输入网站描述"
+                               type="text" value="<?php echo $row['web_title'] ?>">
                     </div>
                     <div class="mb-4">
-                        <label class="form-label">网站描述</label>
-                        <input class="form-control form-control-lg" id="des" name="des" placeholder="输入网站描述" type="email" value="">
+                        <label class="form-label" for="des">网站描述</label>
+                        <input class="form-control form-control-lg" id="des" name="des" placeholder="输入网站描述"
+                               type="text" value="<?php echo $row['web_des'] ?>">
                     </div>
                     <div class="mb-4">
-                        <label class="form-label">网站关键词</label>
-                        <input class="form-control form-control-lg" id="key" name="key" placeholder="输入网站关键词，用英文逗号隔开" type="email" value="">
+                        <label class="form-label" for="key">网站关键词</label>
+                        <input class="form-control form-control-lg" id="key" name="key"
+                               placeholder="输入网站关键词，用英文逗号隔开" type="text" value="<?php echo $row['web_key'] ?>">
                     </div>
                     <div class="mb-4">
-                        <button class="btn btn-alt-primary" onclick="" type="submit">
+                        <label class="form-label" for="qq">站长QQ</label>
+                        <input class="form-control form-control-lg" id="qq" name="qq"
+                               placeholder="输入你的QQ号" type="text" value="<?php echo $row['web_qq'] ?>">
+                    </div>
+                    <div class="mb-4">
+                        <label class="form-label" for="beian">网站备案号</label>
+                        <input class="form-control form-control-lg" id="beian" name="beian"
+                               placeholder="如果没有备案可以不用输入" type="text" value="<?php echo $row['web_beian'] ?>">
+                    </div>
+                    <div class="mb-4">
+                        <label class="form-label" for="ban">底部版权</label>
+                        <input class="form-control form-control-lg" id="ban" name="ban"
+                               placeholder="例如CloudZA" type="text" value="<?php echo $row['web_ban']?>">
+                    </div>
+                    <div class="mb-4">
+                        <label class="form-label" for="page_num">后台每页条数</label>
+                        <input class="form-control form-control-lg" id="page_num" name="page_num"
+                               placeholder="最少填10条" type="number" value="<?php echo $row['web_page_nums'] ?>">
+                    </div>
+                    <div class="mb-4">
+                        <button class="btn btn-alt-primary" onclick="submit()" type="submit">
                             提交
                         </button>
                     </div>
                 </div>
             </div>
-        </form>
     </div>
 </div>
+
+<script>
+    function submit ()
+    {
+        x.ajax('ajax.php?act=set_web', {
+            web_title: x.getval('#title'),
+            web_des: x.getval('#des'),
+            web_key: x.getval('#des'),
+            web_qq: x.getval('#qq'),
+            web_beian: x.getval('#beian'),
+            web_ban: x.getval('#ban'),
+            web_page_nums: x.getval('#page_num')
+        }, (data) => {
+            if (data.code === 200) {
+                x.notify(data.msg, 'success')
+                setTimeout(() => {
+                    x.pjax('?set_web')
+                }, 1200)
+            } else {
+                x.notify(data.msg, 'warning')
+            }
+        })
+    }
+</script>
