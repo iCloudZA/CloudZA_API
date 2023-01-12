ace79aae3b8bdb2656ed1dcb2b45cf00ce886cf4
iCloudZA <admin@osuu.cc>
2023-01-12 19:09:24 +0800

    优化配色

--- admin/login.php
+++ admin/login.php
@@ -1,18 +1,19 @@
 <?php
-    /*
-     * @LastEditors: CloudZA(云之安) <admin@osuu.cc>
-     * @hitokoto: 一场秋雨一场凉，秋心酌满泪为霜。
-     * Copyright (c) 2022 by CloudZA, All Rights Reserved.
-     */
+/*
+ * @LastEditors: CloudZA(云之安) <admin@osuu.cc>
+ * @hitokoto: 一场秋雨一场凉，秋心酌满泪为霜。
+ * Copyright (c) 2022 by CloudZA, All Rights Reserved.
+ */
 
-    require_once '../include/common.php';
-    $WEB = Db::table('webset')->select();
+require_once '../include/common.php';
+$WEB = Db::table('webset')->select();
 ?>
 <html lang="zh-cn">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-    <title><?php echo $WEB[ 0 ][ 'title' ]; ?> - 后台登录</title>
+    <title><?php
+        echo $WEB[ 0 ][ 'title' ]; ?> - 后台登录</title>
     <link rel="icon" href="../assets/img/favicons/favicon.png" />
     <link rel="stylesheet" id="css-main" href="../assets/css/codebase.min-5.4.css" />
 </head>
@@ -36,35 +37,36 @@
                                 It’s a great day today!
                             </h2>
                         </div>
-                        <div
-                        ="js-validation-signin">
-                        <div class="block block-themed block-rounded block-fx-shadow">
-                            <div class="block-header bg-gd-dusk">
-                                <h3 class="block-title">请登录</h3>
-                            </div>
-                            <div class="block-content">
-                                <div class="form-floating mb-4">
-                                    <input type="text" class="form-control" name="user" placeholder="输入你的账号"
-                                           required />
-                                    <label class="form-label" for="login-username">账号</label>
+                        <div class="js-validation-signin">
+                            <div class="block block-themed block-rounded block-fx-shadow">
+                                <div class="block-header bg-primary-light">
+                                    <h3 class="block-title">请登录</h3>
                                 </div>
-                                <div class="form-floating mb-4">
-                                    <input type="password" class="form-control" name="password"
-                                           placeholder="输入你的密码" required />
-                                    <label class="form-label" for="login-password">密码</label>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-6 d-sm-flex align-items-center push">
-                                        <div class="form-check">
-                                            <input type="checkbox" class="form-check-input" checked="" value="y"
-                                                   required />
-                                            <label class="form-check-label" for="ok">记住我</label>
-                                        </div>
+                                <div class="block-content">
+                                    <div class="form-floating mb-4">
+                                        <input type="text" class="form-control" name="user" placeholder="输入你的账号"
+                                               required />
+                                        <label class="form-label" for="login-username">账号</label>
                                     </div>
-                                    <div class="col-sm-6 text-sm-end push">
-                                        <button type="submit" id="submit" class="btn btn-lg btn-alt-primary fw-medium">
-                                            登录
-                                        </button>
+                                    <div class="form-floating mb-4">
+                                        <input type="password" class="form-control" name="password"
+                                               placeholder="输入你的密码" required />
+                                        <label class="form-label" for="login-password">密码</label>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-6 d-sm-flex align-items-center push">
+                                            <div class="form-check">
+                                                <input type="checkbox" class="form-check-input" checked="" value="y"
+                                                       required />
+                                                <label class="form-check-label" for="ok">记住我</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 text-sm-end push">
+                                            <button type="submit" id="submit"
+                                                    class="btn btn-lg btn-alt-primary fw-medium">
+                                                登录
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -73,8 +75,7 @@
                 </div>
             </div>
         </div>
-</div>
-</main>
+    </main>
 </div>
 <script src="../assets/js/codebase.app.min-5.4.js"></script>
 <script src="../assets/js/lib/jquery.min.js"></script>
