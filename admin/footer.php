99baf8b52773108ee49852a0c1beb84f428d5da7
iCloudZA <admin@osuu.cc>
2023-01-13 21:12:35 +0800

    优化

--- admin/footer.php
+++ admin/footer.php
@@ -4,11 +4,11 @@
     <div class="content py-3">
         <div class="row fs-sm">
             <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-end">
-                Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="./"
-                                                                           target="_blank">CloudZA</a>
+                Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="https://osuu.cc/" target="_blank">CloudZA</a>
             </div>
             <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-start">
-                <a class="fw-semibold" href="https://1.envato.market/95j" target="_blank">云之安 </a> &copy; 2022-<span
+                <a class="fw-semibold" href="https://1.envato.market/95j" target="_blank"><?php
+                    echo FOOTER_BAN ?> </a> &copy; <span
                         data-toggle="year-copy"></span>
             </div>
         </div>
@@ -28,7 +28,7 @@
 
 <script>
     $(function () {
-        jQuery(function () {
+        $(function () {
             Codebase.helpers('jq-slimscroll');
         });
     })
