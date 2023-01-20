109a4a5f9deec37314bb90f8d8a2def1c99357df
iCloudZA <admin@osuu.cc>
2023-01-20 23:33:46 +0800

    完善图表

--- extend/adm/index.php
+++ extend/adm/index.php
@@ -1,14 +1,14 @@
 <?php
 if ( !isset($islogin)) header("Location: /"); //非法访问拦截
-//$weekarray=["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
-//echo $weekarray[date("w",strtotime("2018-10-6"))];
-//echo $weekarray[date("w",time())];
-
+$apiCount = Db::table('api_list')->count();
+$apiCallCount = Db::table('api_count')->count();
+$today = Db::table('api_count')->where('datetime','like','%'.date('Y-m-d').'%')->count();
+//$todayChampion = Db::query('SELECT name, COUNT(*) as count FROM api_count WHERE DATE(datetime) = CURDATE() GROUP BY name ORDER BY count DESC LIMIT 1;');
 
 ?>
 
 <div class="block block-rounded bg-primary-light mb-4">
-    <div class="block-content bg-white-5">
+    <div class="bl  ock-content bg-white-5">
         <div class="py-4 text-center">
             <h1 class="h2 fw-bold text-white mb-2"><?php
                 echo TITLE ?></h1>
@@ -19,54 +19,54 @@ if ( !isset($islogin)) header("Location: /"); //非法访问拦截
 </div>
 
 <div class="row">
-    <div class="col-6 col-xl-3">
+    <div class="col-6 col-xl-4">
         <a class="block block-rounded block-link-pop text-end" href="javascript:void(0)">
             <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center border-black-op-b border-3">
                 <div class="d-none d-sm-block">
                     <i class="si si-bar-chart fa-2x text-primary-light"></i>
                 </div>
                 <div class="text-end">
-                    <div class="fs-3 fw-semibold text-primary">435</div>
-                    <div class="fs-sm fw-semibold text-uppercase text-muted">用户数量</div>
+                    <div class="fs-3 fw-semibold text-primary"><?php echo $apiCount ?><span class="fs-sm">个</span></div>
+                    <div class="fs-sm fw-semibold text-uppercase text-muted">接口数量</div>
                 </div>
             </div>
         </a>
     </div>
-    <div class="col-6 col-xl-3">
-        <a class="block block-rounded block-link-pop text-end" href="javascript:void(0)">
-            <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center border-black-op-b border-3">
-                <div class="d-none d-sm-block">
-                    <i class="si si-trophy fa-2x text-earth-light"></i>
-                </div>
-                <div class="text-end">
-                    <div class="fs-3 fw-semibold text-earth">177</div>
-                    <div class="fs-sm fw-semibold text-uppercase text-muted">API接口</div>
-                </div>
-            </div>
-        </a>
-    </div>
-    <div class="col-6 col-xl-3">
+<!--    <div class="col-6 col-xl-3">-->
+<!--        <a class="block block-rounded block-link-pop text-end" href="javascript:void(0)">-->
+<!--            <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center border-black-op-b border-3">-->
+<!--                <div class="d-none d-sm-block">-->
+<!--                    <i class="si si-trophy fa-2x text-earth-light"></i>-->
+<!--                </div>-->
+<!--                <div class="text-end">-->
+<!--                    <div class="fs-3 fw-semibold text-earth"><span class="fs-lg">--><?php //echo $todayChampion[0]['name'] ?><!--</span></div>-->
+<!--                    <div class="fs-sm fw-semibold text-uppercase text-muted">今日冠军</div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </a>-->
+<!--    </div>-->
+    <div class="col-6 col-xl-4">
         <a class="block block-rounded block-link-pop text-end" href="javascript:void(0)">
             <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center border-black-op-b border-3">
                 <div class="d-none d-sm-block">
                     <i class="si si-bar-chart fa-2x text-elegance-light"></i>
                 </div>
                 <div class="text-end">
-                    <div class="fs-3 fw-semibold text-elegance">10930</div>
+                    <div class="fs-3 fw-semibold text-elegance"><?php echo $apiCallCount; ?><span class="fs-sm">次</span></div>
                     <div class="fs-sm fw-semibold text-uppercase text-muted">调用次数</div>
                 </div>
             </div>
         </a>
     </div>
-    <div class="col-6 col-xl-3">
+    <div class="col-6 col-xl-4">
         <a class="block block-rounded block-link-pop text-end" href="javascript:void(0)">
             <div class="block-content block-content-full d-sm-flex justify-content-between align-items-center border-black-op-b border-3">
                 <div class="d-none d-sm-block">
                     <i class="si si-fire fa-2x text-corporate-light"></i>
                 </div>
                 <div class="text-end">
-                    <div class="fs-3 fw-semibold text-pulse">1230</div>
-                    <div class="fs-sm fw-semibold text-uppercase text-muted">日均IP</div>
+                    <div class="fs-3 fw-semibold text-pulse"><?php echo $today; ?><span class="fs-sm">次</span></div>
+                    <div class="fs-sm fw-semibold text-uppercase text-muted">今日调用</div>
                 </div>
             </div>
         </a>
@@ -94,29 +94,29 @@ if ( !isset($islogin)) header("Location: /"); //非法访问拦截
 </div>
 <div class="row">
 
-    <div class="col-xl-6">
-        <div class="block block-rounded">
-            <div class="block-header block-header-default">
-                <i class="si si-graph me-1 text-muted"></i>
-                <h3 class="block-title">用户趋势</h3>
-                <div class="block-options">
-                    <button type="button" class="btn-block-option" data-toggle="block-option" data-action="state_toggle"
-                            data-action-mode="demo">
-                        <i class="si si-refresh"></i>
-                    </button>
-                </div>
-            </div>
-            <div class="block-content block-content-full text-center">
-                <canvas id="js-chartjs-lines"></canvas>
-            </div>
-        </div>
-    </div>
+<!--    <div class="col-xl-6">-->
+<!--        <div class="block block-rounded">-->
+<!--            <div class="block-header block-header-default">-->
+<!--                <i class="si si-graph me-1 text-muted"></i>-->
+<!--                <h3 class="block-title">用户趋势</h3>-->
+<!--                <div class="block-options">-->
+<!--                    <button type="button" class="btn-block-option" data-toggle="block-option" data-action="state_toggle"-->
+<!--                            data-action-mode="demo">-->
+<!--                        <i class="si si-refresh"></i>-->
+<!--                    </button>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="block-content block-content-full text-center">-->
+<!--                xxxxxx-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 
-    <div class="col-xl-6">
+    <div class="col-xl-12">
         <div class="block block-rounded">
             <div class="block-header block-header-default">
                 <h3 class="block-title">
-                        <i class="si si-bar-chart me-1 text-muted"></i>API统计
+                    <i class="si si-bar-chart me-1 text-muted"></i>API统计
                 </h3>
                 <div class="block-options">
                     <button type="button" class="btn-block-option" data-toggle="block-option" data-action="state_toggle"
