e73e7808320e8cf8505c40a09c558168560cddbc
iCloudZA <admin@osuu.cc>
2023-01-21 16:03:07 +0800

    优化

--- admin/footer.php
+++ admin/footer.php
@@ -4,7 +4,8 @@
     <div class="content py-3">
         <div class="row fs-sm">
             <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-end">
-                Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="https://osuu.cc/" target="_blank">CloudZA</a>
+                Crafted with <i class="fa fa-heart text-danger"></i> by
+                <a class="fw-semibold" href="https://osuu.cc/" target="_blank">CloudZA</a>
             </div>
             <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-start">
                 <a class="fw-semibold" href="https://1.envato.market/95j" target="_blank"><?php
@@ -26,20 +27,14 @@
 <script src="../assets/js/app.min.js"></script>
 
 <script>
-
-    $(function () {
-        $(function () {
-            Codebase.helpers('jq-slimscroll');
-        });
-    })
     // ChartJS
-    x.ajax('index.php?api=base',{},(data)=>{
-        console.log('CloudZA API [BASE] =>',data)
-        if(data.code === '200'){
+    x.ajax('index.php?api=base', {}, (data) => {
+        console.log('CloudZA API [BASE] =>', data)
+        if (data.code === '200') {
             let chart = data.data.chart
             let event = data.data.eventList
-            x.barCharts(chart.date,chart.pv,chart.ip);
-            $.each(event,(index, item) => {
+            x.barCharts(chart.date, chart.pv, chart.ip);
+            $.each(event, (index, item) => {
                 $("#eventList").append(
                     "<li>" +
                     "<i class='" + item.icon + " " + item.colour + "'></i>" +
