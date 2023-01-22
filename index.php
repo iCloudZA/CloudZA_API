8f41cc6cbbe96465f38fe0513922bcc1338d1b7d
iCloudZA <admin@osuu.cc>
2023-01-22 16:51:25 +0800

    优化

--- index.php
+++ index.php
@@ -21,11 +21,11 @@ function SafeFilter (&$arr): void
     if (is_array($arr)) {
         foreach ($arr as $key => $value) {
             if ( !is_array($value)) {
-                if ( !ini_get('magic_quotes_gpc')) {             //不对magic_quotes_gpc转义过的字符使用addslashes(),避免双重转义。
-                    $value = addslashes($value);           //给单引号（'）、双引号（"）、反斜线（\）与 NUL（NULL 字符）加上反斜线转义
+                if ( !ini_get('magic_quotes_gpc')) {
+                    $value = addslashes($value);
                 }
-                $value = preg_replace($ra , '' , $value);     //删除非打印字符，粗暴式过滤xss可疑字符串
-                $arr[ $key ] = htmlentities(strip_tags($value)); //去除 HTML 和 PHP 标记并转换为 HTML 实体
+                $value = preg_replace($ra , '' , $value);
+                $arr[ $key ] = htmlentities(strip_tags($value));
             } else {
                 SafeFilter($arr[ $key ]);
             }
